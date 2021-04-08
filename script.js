7294405ca4db554e103201c2f7da4920ef453547
Shukurah <bakareshukurah@outlook.com>
2021-04-08 12:15:51 +0100

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Alert Welcome
+function welcomeUser() {
+  alert ('Welcome');
+}             
+window.addEventListener('load',welcomeUser);
+// Form Submit button
+const submitButton = document.querySelector('.submitButton')
+const onSubmit= () => {
+  submitButton
+}
